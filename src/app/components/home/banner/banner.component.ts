127240eab4c59d8dcbde662a5cbadbabb8cc656b
Mohsin-424 <mohsinpu24@gmail.com>
2024-09-01 14:22:57 +0500

    Mohsin

--- src/app/components/home/banner/banner.component.ts
+++ src/app/components/home/banner/banner.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
-import {trigger, state, style, animate, transition, stagger, query } from "@angular/animations"
-import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  stagger,
+  query,
+} from "@angular/animations";
+import { AnalyticsService } from "src/app/services/analytics/analytics.service";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss'],
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.scss"],
   animations: [
-    trigger('bannerTrigger', [
+    trigger("bannerTrigger", [
       transition(":enter", [
         query("*", [
           style({ opacity: 0, transform: "translateX(-50px)" }),
@@ -16,23 +24,15 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
             animate(
               "250ms cubic-bezier(0.35, 0, 0.25, 1)",
               style({ opacity: 1, transform: "none" })
-            )
-          ])
-        ])
-      ])
-    ])
-  ]
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class BannerComponent implements OnInit {
+  constructor(public analyticsService: AnalyticsService) {}
 
-  
-
-  constructor(
-    public analyticsService: AnalyticsService
-  ) { }
-
-  ngOnInit(): void { 
-  }
-  
-
+  ngOnInit(): void {}
 }
